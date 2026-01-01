82cacab15ec5bbd728bf99285683221ad32d9aee
Pranav Uttarwar <uttarwar,pranav@gmail.com>
2026-01-01 21:42:10 +0530

    Setup

---
+++ S1_Steup/Open Browser.py
@@ -0,0 +1,9 @@
+import time
+
+from selenium import webdriver
+
+#d=webdriver.Chrome() #Open Chrome Browser
+#d=webdriver.Edge() #Open Edge Browser
+d=webdriver.Firefox() #Open FireFox Browser
+
+time.sleep(5)
