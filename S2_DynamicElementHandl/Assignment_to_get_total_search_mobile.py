a73a88de8c2eb42b42a6c5d724062855e74087e8
pranavuttarwar <uttarwar.pranav@gmail.com>
2026-02-14 14:22:10 +0530

    few files

--- S2_DynamicElementHandl/Assignment_to_get_total_search_mobile.py
+++ S2_DynamicElementHandl/Assignment_to_get_total_search_mobile.py
@@ -13,16 +13,20 @@ search=(driver.find_element(By.XPATH,"(//input[@name='q'])[1]"))
 search.send_keys("Redmi 12G")
 search.send_keys(Keys.ENTER)
 
+#get all searched mobiles
 AllMobName=driver.find_elements(By.XPATH,"//div[@class='jIjQ8S']//div[@class='RG5Slk']")
-print(len(AllMobName))
+print("Total Mobile Result: ",len(AllMobName))
 for name in AllMobName:
     print(name.text)
 
+#Get all mobile price
+print("-------------------------------")
 PriceofMobile=driver.find_elements(By.XPATH,"//div[@class='col col-5-12 mao5dl']//div[@class='oFEPlD']//div[@class='hZ3P6w DeU9vF']")
-print(len(PriceofMobile))
+print("Search mobile price: ",len(PriceofMobile))
 
 for price in PriceofMobile:
     print(price.text)
     time.sleep(1)
+
 
 time.sleep(10)
