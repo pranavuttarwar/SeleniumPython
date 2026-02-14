3ea7a8006a462f487f596b7dcf001fd0803552cf
pranavuttarwar <uttarwar.pranav@gmail.com>
2026-02-14 14:31:32 +0530

    Assignment sum of mobile

---
+++ S2_DynamicElementHandl/AssignmentSumOfAllMobilePrice.py
@@ -0,0 +1,39 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
+driver=webdriver.Chrome()
+driver.implicitly_wait(2)
+driver.get("https://www.flipkart.com/")
+driver.maximize_window()
+
+search=(driver.find_element(By.XPATH,"(//input[@name='q'])[1]"))
+search.send_keys("Redmi 12G")
+search.send_keys(Keys.ENTER)
+
+#get all searched mobiles
+AllMobName=driver.find_elements(By.XPATH,"//div[@class='jIjQ8S']//div[@class='RG5Slk']")
+print("Total Mobile Result: ",len(AllMobName))
+for name in AllMobName:
+    print(name.text)
+
+#Get all mobile price
+print("-------------------------------")
+PriceofMobile=driver.find_elements(By.XPATH,"//div[@class='col col-5-12 mao5dl']//div[@class='oFEPlD']//div[@class='hZ3P6w DeU9vF']")
+print("Search mobile price: ",len(PriceofMobile))
+
+sum=0
+for price in PriceofMobile:
+    mobprice=price.text
+    mobprice = mobprice.replace("₹", "").replace(",", "")
+    mobprice = int(mobprice)
+    print(mobprice)
+    sum=sum+mobprice
+    time.sleep(1)
+
+print("All mobile price sum",sum)
+
+
+time.sleep(10)
